7d81ac8971374f30895d4b08f601f89eecc02147
Rob Lohmann <roblohman@gmail.com>
2019-10-28 11:44:40 +0100

    added logger to write to console and updated readme

---
+++ CodeCube.TestFixtures.XUnit/XunitLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace CodeCube.TestFixtures.XUnit
+{
+    public sealed class XunitLogger : ILogger, IDisposable
+    {
+        private ITestOutputHelper _output;
+
+        public XunitLogger(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            _output.WriteLine(state.ToString());
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            throw new NotSupportedException("Dispose is currently not supported!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CodeCube.TestFixtures.XUnit
+
+Simple extension on the XUnit logging framework to capture everything written to the console.
+This will then be displayed as output in test testresults window.
+
+## How to use (.NET Core)
+1. In the constructor for your unit test add an parameter of type ITestOutputHelper and store it in your unit test class.
+2. In your unit test mock the ILoggerFactory with the following line of code 'var mockLoggerFactory = new Mock<ILoggerFactory>();'
+3. Add the following setup line to your mockLoggerFactory;
+ ''''loggerFactory.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(new XunitLogger(_output));
